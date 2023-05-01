29aefc769bc926541c18734fdd9873ad51d56b9b
Jenifer19IFC <jenifergoedert10@gmail.com>
2023-05-01 10:52:56 -0300

    commit mutex

---
+++ src/ExclusaoMutua/Mutex.java
@@ -0,0 +1,47 @@
+package ExclusaoMutua;
+
+import java.util.ArrayList;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Mutex {
+    static ReentrantLock mutex = new ReentrantLock();
+    static ArrayList<String> listaCompartilhada = new ArrayList<String>(); //Recurso compartilhado
+
+    public static void main(String[] args) {
+        int numThreads = 5;
+        Thread[] threads = new Thread[numThreads];
+        int contador = 1;
+
+        // Cria as threads
+        for (int i = 0; i < numThreads; i++) {
+            final int id = contador;
+            threads[i] = new Thread(new Runnable() {
+                public void run() {
+                	
+                    mutex.lock();
+                    System.out.println("Thread " + id + " entrou na seção crítica");
+                    
+                    listaCompartilhada.add("Elemento " + id);
+                    System.out.println("Lista compartilhada: " + listaCompartilhada);
+
+                    mutex.unlock();
+                    System.out.println("Thread " + id + " saiu da seção crítica");
+                }
+            });
+
+            // Inicia a execução de uma nova thread 
+            threads[i].start();
+            contador++;
+        }
+
+        // Espera as threads terminarem
+        for (int i = 0; i < numThreads; i++) {
+            try {
+                threads[i].join(); // Espera outras threads terminarem
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        
+    }
+}
